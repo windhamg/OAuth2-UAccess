be7e40bdd007c5359952aba38d2dc78bfb1c01a0
Gary Windham <windhamg@email.arizona.edu>
2013-06-12 14:32:19 -0700

    Initial Commit

---
+++ GTMOAuth2Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GTMOAuth2Demo
+//
+//  Created by Gary on 6/12/13.
+//  Copyright (c) 2013 Gary Windham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GTMOAuth2Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GTMOAuth2Demo
+//
+//  Created by Gary on 6/12/13.
+//  Copyright (c) 2013 Gary Windham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
